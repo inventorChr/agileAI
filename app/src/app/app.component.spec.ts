6dd65c19e1c8f1a4f7422c867cdfce99877d47f9
Chris Dionne <c.dionne11@icloud.com>
2023-08-17 23:18:07 -0500

    renamed agileai dir to app

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -15,13 +15,13 @@ describe('AppComponent', () => {
   it(`should have as title 'agileai'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('agileai');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('agileai app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
   });
 });
